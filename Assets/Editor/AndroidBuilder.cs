7daf8df0db16f9ec75a65c403a443c1d148e6737
kyungmin.im <im7072@naver.com>
2017-06-06 23:11:21 +0900

    Test

---
+++ Assets/Editor/AndroidBuilder.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+
+public class AndroidBuilder
+{
+    [MenuItem("Custom/CI/Build Android")]
+    public static void Build()
+    {
+        var scenes = BuildFunction.FindEnabledEditorScenes();
+        BuildFunction.GenericBuild(scenes, "Equality2.apk", BuildTargetGroup.Android, BuildTarget.Android, BuildOptions.None);
+    }
+}
---
+++ Assets/Editor/BuildFunction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+
+
+public class BuildFunction
+{
+    public static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorSceneList = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (scene.enabled == false)
+            {
+                continue;
+            }
+
+            EditorSceneList.Add(scene.path);
+        }
+
+        return EditorSceneList.ToArray();
+    }
+
+    public static void GenericBuild(string[] scenes, string target_filename, BuildTargetGroup build_group, BuildTarget build_target, BuildOptions build_options)
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(build_group, build_target);
+
+        string res = BuildPipeline.BuildPlayer(scenes, target_filename, build_target, build_options);
+        if (0 < res.Length)
+        {
+            throw new Exception("GenericBuild failure: " + res);
+        }
+    }
+}
